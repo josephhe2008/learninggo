6a46efb8d3c9dd55651f661fd67c711717fd23f3
Joseph He <joseph.he.2008@gmail.com>
2023-08-31 16:46:46 -0500

    package functionsn not methods.
    The string in question is the first argument

---
+++ go_by_example/string_functions.go
@@ -0,0 +1,26 @@
+package main
+import (
+    "fmt"
+    s "strings" // alias strings to s
+)
+
+
+// alias fmt.Println to p
+var p = fmt.Println
+func main() {
+
+	// string functions from the package, not the methods
+	// always pass the string as the 1st argument
+    p("Contains:  ", s.Contains("test", "es"))
+    p("Count:     ", s.Count("test", "t"))
+    p("HasPrefix: ", s.HasPrefix("test", "te"))
+    p("HasSuffix: ", s.HasSuffix("test", "st"))
+    p("Index:     ", s.Index("test", "e"))
+    p("Join:      ", s.Join([]string{"a", "b"}, "-"))
+    p("Repeat:    ", s.Repeat("a", 5))
+    p("Replace:   ", s.Replace("foo", "o", "0", -1))
+    p("Replace:   ", s.Replace("foo", "o", "0", 1))
+    p("Split:     ", s.Split("a-b-c-d-e", "-"))
+    p("ToLower:   ", s.ToLower("TEST"))
+    p("ToUpper:   ", s.ToUpper("test"))
+}
