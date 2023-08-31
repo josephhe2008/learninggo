4cd9262b6edd9c70c29598c156cb677237a5c5c1
Joseph He <joseph.he.2008@gmail.com>
2023-08-31 00:21:47 -0500

    use a limiter channel to control the pace

---
+++ go_by_example/rate_limiting.go
@@ -0,0 +1,43 @@
+package main
+import (
+    "fmt"
+    "time"
+)
+func main() {
+
+    requests := make(chan int, 5)
+    for i := 1; i <= 5; i++ {
+        requests <- i
+    }
+    close(requests)
+
+    limiter := time.Tick(200 * time.Millisecond)
+
+    for req := range requests {
+        <-limiter
+        fmt.Println("request", req, time.Now())
+    }
+
+	burstyLimiter := make(chan time.Time, 5)
+
+    for i := 0; i < 2; i++ {
+        burstyLimiter <- time.Now()
+    }
+
+	go func() {
+		for t := range time.Tick(200 * time.Millisecond) {
+			burstyLimiter <- t
+		}
+
+	}()
+
+	burstRequests := make(chan int, 5)
+	for i := 1; i <= 5; i++ {
+		burstRequests <- i
+	}
+	close(burstRequests)
+	for req := range burstRequests {
+		l := <- burstyLimiter
+		fmt.Println("request", req, time.Now(), l)
+	}
+}
