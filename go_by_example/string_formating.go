9ae9ca9a51d0f7b5686ee176f6b65274d8e209a2
Joseph He <joseph.he.2008@gmail.com>
2023-08-31 17:05:37 -0500

    format string

---
+++ go_by_example/string_formating.go
@@ -0,0 +1,36 @@
+package main
+import (
+    "fmt"
+    "os"
+)
+type point struct {
+    x, y int
+}
+func main() {
+    p := point{1, 2}
+    fmt.Printf("struct1: %v\n", p)
+    fmt.Printf("struct2: %+v\n", p) // include the field names
+    fmt.Printf("struct3: %#v\n", p) // main.point{x:1, y:2}
+
+    fmt.Printf("type: %T\n", p)
+    fmt.Printf("bool: %t\n", true) // %t for boolean value
+    fmt.Printf("int: %d\n", 123)
+    fmt.Printf("bin: %b\n", 14) // %b is binary
+    fmt.Printf("char: %c\n", 33) //%c is character
+    fmt.Printf("hex: %x\n", 456) //%x is hex 16
+    fmt.Printf("float1: %f\n", 78.9) //default to 78.900000
+    fmt.Printf("float2: %e\n", 123400000.0)
+    fmt.Printf("float3: %E\n", 123400000.0)
+    fmt.Printf("str1: %s\n", "\"string\"")
+    fmt.Printf("str2: %q\n", "\"string\"")
+    fmt.Printf("str3: %x\n", "hex this")
+    fmt.Printf("pointer: %p\n", &p)
+    fmt.Printf("width1: |%6d|%6d|\n", 12, 345)
+    fmt.Printf("width2: |%6.2f|%6.2f|\n", 1.2, 3.45)
+    fmt.Printf("width3: |%-6.2f|%-6.2f|\n", 1.2, 3.45)
+    fmt.Printf("width4: |%6s|%6s|\n", "foo", "b")
+    fmt.Printf("width5: |%-6s|%-6s|\n", "foo", "b")
+    s := fmt.Sprintf("sprintf: a %s", "string")
+    fmt.Println(s)
+    fmt.Fprintf(os.Stderr, "io: an %s\n", "error")
+}
