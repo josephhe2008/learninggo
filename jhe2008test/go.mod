a45c8e73d7ec7e865dce122e5022a166c2aef4b3
Joseph He <joseph.he.2008@gmail.com>
2023-08-31 21:47:14 -0500

    fix syntax error

---
+++ jhe2008test/go.mod
@@ -0,0 +1,5 @@
+module testjhe2008
+
+go 1.21.0
+
+require github.com/josephhe2008/jhe2008 v0.0.0-20230901023346-312c8adba6e8
---
+++ jhe2008test/testjhe2008_github.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/josephhe2008/jhe2008"
+)
+
+func main() {
+	
+	str := jhe2008.Jhe2008()
+	fmt.Println(str)
+
+}
